3438032a193727149c4aaf934aa088212fc27b9f
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-01-29 09:49:25 +0100

    comit  master 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
     //comienzo a programar
-        System.out.println("fin de la funcionalidad ");
+        System.out.println("fin de la funcionalidad 2 ");
 
     }
 }
